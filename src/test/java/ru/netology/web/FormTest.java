6387919599cacf29787ce9c6b1a983724dc2e7c1
neshutnick <neshutnick89@gmail.com>
2025-07-18 01:17:02 +0300

    Tests fix

--- src/test/java/ru/netology/web/FormTest.java
+++ src/test/java/ru/netology/web/FormTest.java
@@ -6,27 +6,18 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import java.time.Duration;
 
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class FormTest {
-    @BeforeAll
-    static void setUpAll() {
-        Configuration.headless = true;
-        Configuration.browser = "chrome";
-    }
-
+   
     @BeforeEach
     void setUp() {
         open("http://localhost:7777");
     }
-
-    @AfterEach
-    void tearDown() {
-    }
-
 
     @Test // Все поля заполненые корректно, чекбокс отмечен
     void shouldSubmitRequestSuccessfully() {
@@ -39,12 +30,14 @@ class FormTest {
         $("[data-test-id=phone] input").setValue("+79270000000");
         $("[data-test-id=agreement]").click();
         $("button.button").click();
-        $("[data-test-id=notification]").should(appear, java.time.Duration.ofSeconds(15));
-        $("[data-test-id=notification] .notification__title").shouldHave(text("Успешно!"));
+        $("[data-test-id=notification] .notification__content")
+            .shouldBe(visible, Duration.ofSeconds(15))
+            .shouldHave(exactText("Встреча успешно забронирована на " + date));
     }
 
     @Test // Поле город пустое
     void shouldShowErrorIfCityIsEmpty() {
+
         String date = DataGenerator.generateDate(3);
         var dateInput = $("[data-test-id=date] input");
         dateInput.doubleClick().sendKeys(date);
@@ -52,8 +45,9 @@ class FormTest {
         $("[data-test-id=phone] input").setValue("+79270000000");
         $("[data-test-id=agreement]").click();
         $("button.button").click();
-        String errorText = $("[data-test-id=city].input_invalid .input__sub").getText();
-        assertEquals("Поле обязательно для заполнения", errorText.trim());
+        $("[data-test-id=city].input_invalid .input__sub")
+            .shouldBe(visible)
+            .shouldHave(exactText("Поле обязательно для заполнения"));
     }
 
     @Test // Поле имя пустое
@@ -67,8 +61,9 @@ class FormTest {
         $("[data-test-id=phone] input").setValue("+79270000000");
         $("[data-test-id=agreement]").click();
         $("button.button").click();
-        String errorText = $("[data-test-id=name].input_invalid .input__sub").getText();
-        assertEquals("Поле обязательно для заполнения", errorText.trim());
+        $("[data-test-id=name].input_invalid .input__sub")
+            .shouldBe(visible)
+            .shouldHave(exactText("Поле обязательно для заполнения"));
     }
 
     @Test // поле номер пустое
@@ -82,8 +77,9 @@ class FormTest {
         $("[data-test-id=phone] input").setValue("");
         $("[data-test-id=agreement]").click();
         $("button.button").click();
-        String errorText = $("[data-test-id=phone].input_invalid .input__sub").getText();
-        assertEquals("Поле обязательно для заполнения", errorText.trim());
+        $("[data-test-id=phone].input_invalid .input__sub")
+            .shouldBe(visible)
+            .shouldHave(exactText("Поле обязательно для заполнения"));
     }
 
     @Test // чекбокс не отмечен
@@ -110,8 +106,9 @@ class FormTest {
         $("[data-test-id=phone] input").setValue("+79270000000");
         $("[data-test-id=agreement]").click();
         $("button.button").click();
-        String errorText = $("[data-test-id=name].input_invalid .input__sub").getText();
-        assertEquals("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.", errorText.trim());
+        $("[data-test-id=name].input_invalid .input__sub")
+            .shouldBe(visible)
+            .shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
     }
 
     @Test // телефон заполнен некорректно
@@ -125,20 +122,24 @@ class FormTest {
         $("[data-test-id=phone] input").setValue("7634234234532");
         $("[data-test-id=agreement]").click();
         $("button.button").click();
-        String errorText = $("[data-test-id=phone].input_invalid .input__sub").getText();
-        assertEquals("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.", errorText.trim());
+        $("[data-test-id=phone].input_invalid .input__sub")
+            .shouldBe(visible)
+            .shouldHave(exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
     }
 
     @Test // город не из списка административных центров
     void shouldShowErrorIfCityIsUnavailable() {
+
         $("[data-test-id=city] input").setValue("Вашингтон");
         String date = DataGenerator.generateDate(3);
         var dateInput = $("[data-test-id=date] input");
         dateInput.doubleClick().sendKeys(date);
-        $("[data-test-id=name] input").setValue("Кондраторов");
-        $("[data-test-id=phone] input").setValue("+79047749014");
+        $("[data-test-id=name] input").setValue("Иванов-Петров Иван");
+        $("[data-test-id=phone] input").setValue("+79270000000");
         $("[data-test-id=agreement]").click();
         $("button.button").click();
-        $("[data-test-id=city] .input__sub").shouldHave(text("Доставка в выбранный город недоступна"));
+        $("[data-test-id=city] .input__sub")
+             .shouldBe(visible)
+             .shouldHave(text("Доставка в выбранный город недоступна"));
     }
 }
